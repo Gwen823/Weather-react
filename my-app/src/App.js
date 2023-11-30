b08715fc29d8917c78a2ac924c52370010c67f0c
Gwen823 <132462508+Gwen823@users.noreply.github.com>
2023-11-30 17:37:48 -0500

    added bootstrap

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,12 +10,15 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <div className="App">
+      <div className="container">
       <h1>React Weather App</h1>
       <WeatherReact />
-    </div>
+    
     <div>
       <ul></ul>
     </div>
     <footer>coded by Johana Abreu <a href="https://github.com/Gwen823/Weather-react.git" target="_blank">open source Github</a></footer>
+    </div>
+    </div>
   </StrictMode>
 );
