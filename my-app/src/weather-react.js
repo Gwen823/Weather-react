afffe9ddba1f52506de04041b0443cf565af9a51
Gwen823 <132462508+Gwen823@users.noreply.github.com>
2023-12-06 16:38:45 -0500

    weather info in a new folder

---
+++ my-app/src/weather-react.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import weatherInfo from "./weatherInfo";
+import weatherInfo from "./weatherInfo";
+import axios from "axios";
+import "./Weather.css";
+
+export default function WeatherReact() {
+ const[watherData,setWeatherData]=useState({ready:false});
+ const[city,setCity]=useState(props.defaultCity)
+function handleResponse(response){
+    setWeather({
+        ready: true,
+        city: response.data.main,
+        date:new Date (response.data.dt*100),
+        temperature: response.data.main.temp,
+        description: response.data.weather[0].description,
+        wind: response.data.wind.speed,
+        humidity: response.data.main.humidity,
+        icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+      };
+    setLoaded(true);
+}
+
+function search(){
+  const apiKey="bb0df6985c2eab6a171d64a6bacbb4e1";
+  let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+
+}
+function handleSubmit(event){
+  event.preventDefault();
+}
+
+function handleCityChange(event){
+  setCity(event.target.value);
+  search(city);
+}
+  if (weatherData.ready) {
+    return (
+<div className="Weather">  
+<form onSubmit={handleSubmit}>
+    <div className="row"> 
+    <div className="col-9">
+    <input 
+    type="search"
+     placeholder="Enter a city" 
+     className="form-contol"
+     autoFocus="on"
+     onChange={handleCityChange}/>  
+    </div>
+    <div className="col-3">
+<input 
+type="submit" 
+value="search" 
+className="btn btn-primary w-100"/> 
+</div>
+</div>
+</form>
+<WeatherInfo data={weatherData}/>
+</div>
+    );
+
+  } 
+  else {
+   search();
+    return"loading..."
+  }
+}
---
+++ my-app/src/weatherInfo.js
@@ -0,0 +1,44 @@
+import React from "react";
+import FormatDate from "./FormatDate";
+import FormatDate from "./FormatDate";
+
+export default function weatherInfo(props) {
+
+return(
+
+ <div className="weatherInfo">
+<h1 > {props.data.city} </h1>
+<ul>
+    <li>
+       <formatDate date={props.data.date}/>
+    </li>
+    <img src={props.data.icon} alt="Weather Icon" />
+    <li>{props.data.description}</li>
+</ul>
+<div className="row mt-3">
+<div className="col-6">
+<div className="row">
+    <div className="clearfix"> 
+<img src="{props.data.iconUrl}"
+ alt="{props.data.description}"
+  className="float-left"
+  />
+<div className="float-left">
+<span className="temperature">Temperature {Math.round(weather.temperature)}</span> 
+<span className="unit">C</span>
+</div>
+</div>
+</div>
+<div className="col-6">
+<ul>
+
+      <li className="text-capitalized">Description:{props.data.desciption}</li>
+      <li> Humidity {props.data.humidity}(%) </li>
+      <li> Wind {Math.round(props.data.wind)}(Km/h):</li>
+    </ul>
+    </div>
+</div>
+</div>
+
+  );
+  }
